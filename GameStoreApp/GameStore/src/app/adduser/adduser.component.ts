81df49b629f232b28f38d5ed0162bd877887ab24
Tevin Gray <tjgray94@yahoo.com>
2025-06-01 20:28:16 -0500

    Updated style for login, about, and adduser(w/ validations)

--- GameStoreApp/GameStore/src/app/adduser/adduser.component.ts
+++ GameStoreApp/GameStore/src/app/adduser/adduser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../user';
 import { UserService } from '../user.service';
 
@@ -9,34 +10,59 @@ import { UserService } from '../user.service';
 })
 export class AdduserComponent implements OnInit {
 
+  userForm!: FormGroup;
+  submitted = false;
+  formSubmitted = false;
+
   user: User = {
     name: '',
     email: '',
     password: ''
   };
-  submitted = false;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(): void {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  // Getter for easy access to form fields
+  get f() { return this.userForm.controls; }
+
   saveUser(): void {
+    this.formSubmitted = true;
+    
+    // Stop here if form is invalid
+    if (this.userForm.invalid) {
+      return;
+    }
+
     const data = {
-      name: this.user.name,
-      email: this.user.email,
-      password: this.user.password
+      name: this.f.name.value,
+      email: this.f.email.value,
+      password: this.f.password.value
     };
+    
     this.userService.addUser(data)
-      .subscribe(response => { this.submitted = true })
+      .subscribe(response => { 
+        this.submitted = true;
+      });
   }
 
   newUser(): void {
     this.submitted = false;
-    this.user = {
-      name: '',
-      email: '',
-      password: ''
-    }
+    this.formSubmitted = false;
+    this.userForm.reset();
   }
 }
