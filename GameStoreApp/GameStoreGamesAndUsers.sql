18ee5a0357a27486a7de1483ec43810b4f19692b
Tevin Gray <tjgray94@yahoo.com>
2025-06-15 00:00:40 -0500

    Updated games query to include userId

--- GameStoreApp/GameStoreGamesAndUsers.sql
+++ GameStoreApp/GameStoreGamesAndUsers.sql
@@ -1,13 +1,13 @@
-INSERT INTO games VALUES (1, 'Madden.jpg', 'Madden NFL 2022', 47.99, 4.1, 'August 20, 2021');
-INSERT INTO games VALUES (2, 'NBA2k.jpg', 'NBA 2K23', 35.99, 2.8, 'September 4, 2022');
-INSERT INTO games VALUES (3, 'SanAndreas.jpg', 'Grand Theft Auto: San Andreas', 50.99, 4.2, 'October 26, 2004');
-INSERT INTO games VALUES (4, 'NFL_Street_2.jpg', 'NFL Street 2', 35.99, 3.7, 'December 22, 2004');
-INSERT INTO games VALUES (5, 'DubEdition.jpg', 'Midnight Club 3: Dub Edition', 39.99, 4.8, 'April 11, 2005');
-INSERT INTO games VALUES (6, 'Call_of_Duty.png', 'Call of Duty: World at War', 25.99, 3.5, 'November 11, 2008');
-INSERT INTO games VALUES (7, 'Halo.jpg', 'Halo 3', 35.99, 1.0, 'September 25, 2007');
-INSERT INTO games VALUES (8, 'Spiderman.jpg', 'Spiderman: Miles Morales', 49.99, 3.5, 'November 12, 2020');
-INSERT INTO games VALUES (9, 'NeedForSpeed.jpg', 'Need for Speed Underground 2', 22.99, 3.5, 'November 9, 2004');
-INSERT INTO games VALUES (10, 'Fortnite.jpg', 'Fortnite', 9.99, 2.5, 'July 25, 2017');
+INSERT INTO games VALUES (1, 1, 'Madden.jpg', 'Madden NFL 2022', 47.99, 4.1, 'August 20, 2021');
+INSERT INTO games VALUES (2, 1, 'NBA2k.jpg', 'NBA 2K23', 35.99, 2.8, 'September 4, 2022');
+INSERT INTO games VALUES (3, 1, 'SanAndreas.jpg', 'Grand Theft Auto: San Andreas', 50.99, 4.2, 'October 26, 2004');
+INSERT INTO games VALUES (4, 1, 'NFL_Street_2.jpg', 'NFL Street 2', 35.99, 3.7, 'December 22, 2004');
+INSERT INTO games VALUES (5, 1, 'DubEdition.jpg', 'Midnight Club 3: Dub Edition', 39.99, 4.8, 'April 11, 2005');
+INSERT INTO games VALUES (6, 1, 'Call_of_Duty.png', 'Call of Duty: World at War', 25.99, 3.5, 'November 11, 2008');
+INSERT INTO games VALUES (7, 1, 'Halo.jpg', 'Halo 3', 35.99, 1.0, 'September 25, 2007');
+INSERT INTO games VALUES (8, 1, 'Spiderman.jpg', 'Spiderman: Miles Morales', 49.99, 3.5, 'November 12, 2020');
+INSERT INTO games VALUES (9, 1, 'NeedForSpeed.jpg', 'Need for Speed Underground 2', 22.99, 3.5, 'November 9, 2004');
+INSERT INTO games VALUES (10, 1, 'Fortnite.jpg', 'Fortnite', 9.99, 2.5, 'July 25, 2017');
 SELECT setval(pg_get_serial_sequence('games', 'game_id'), (SELECT MAX(game_id) FROM games));
 
 DELETE FROM games;
