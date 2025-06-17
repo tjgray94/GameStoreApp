f22129f58a16aef2ac123e71eaef8b9d1290ce82
Tevin Gray <tjgray94@yahoo.com>
2025-06-17 13:49:29 -0500

    Updated delete method and game retrieval for game-list

--- GameStoreApp/GameStore/src/app/game-list/game-list.component.ts
+++ GameStoreApp/GameStore/src/app/game-list/game-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from '../game';
 import { GameService } from '../game.service';
@@ -11,16 +11,39 @@ import { GameService } from '../game.service';
 })
 export class GameListComponent implements OnInit, OnDestroy {
 
+  id!: number;
   pageTitle = 'Games';
   imageWidth = 50;
   imageMargin = 2;
   showImage: boolean = false;
   errorMessage!: string;
   sub!: Subscription;
-
+  
   games!: Game[];
   currentGame?: Game;
   currentIndex = -1;
+  
+  constructor(private gameService: GameService,
+              private route: ActivatedRoute,
+              private router: Router) {}
+
+  ngOnInit(): void {
+    // Get userId from route parameters
+    const userId = this.route.snapshot.params['userId'];
+    
+    if (userId) {
+      // Store userId for later use
+      this.id = userId;
+      
+      // Get games for this user
+      this.gameService.getGamesByUserId(userId).subscribe((data) => {
+        this.games = data;
+        this.filteredGames = data;
+      });
+    } else {
+      console.error('User ID not found in route parameters');
+    }
+  }
 
   retrieveGames(): void {
     this.gameService.getAll().subscribe(data => {
@@ -65,37 +88,16 @@ export class GameListComponent implements OnInit, OnDestroy {
 
   filteredGames: Game[] = [];
 
-performFilter(filterBy: string): Game[] {
-  filterBy = filterBy.toLocaleLowerCase();
-  return this.games.filter((game: Game) => 
-    game.name.toLocaleLowerCase().includes(filterBy));
-}
-
-toggleImage(): void {
-  this.showImage = !this.showImage;
-}
-
-  constructor(private gameService: GameService,
-              private router: Router) {}
-
-  // ngOnInit(): void {
-  //   this.sub = this.gameService.getAll().subscribe({
-  //     next: games => {
-  //       this.games = games;
-  //       this.filteredGames = this.games;
-  //     },
-  //     error: err => this.errorMessage = err
-  //   }); 
-  // }
-
-  ngOnInit(): void {
-    // this.getGames();
-    this.gameService.getAll()
-      .subscribe((data) => {
-        this.games = data;
-        this.filteredGames = data;
-      });
+  performFilter(filterBy: string): Game[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.games.filter((game: Game) => 
+      game.name.toLocaleLowerCase().includes(filterBy));
   }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
 
   addGame(name: string, image: string, _price: string, releaseDate: string, _rating: string): void {
     let price = parseInt(_price)
@@ -113,18 +115,28 @@ toggleImage(): void {
       next:(game: any) => this.games = game
     })
   }
-
-  deleteGame(id: string): void {
-    let gameId = parseInt(id)
-    this.gameService.deleteGame(gameId).subscribe({next:(game: any) => this.games = game})
-  }
   
-  delete(id: number | undefined) {
-    this.gameService.delete(id).subscribe(res => {this.games = this.games.filter(game => game.gameId !==id)})
-  }
-
-  gotoDetails(game: Game): void {
-    this.router.navigate(['/api/games', game.gameId])
+  delete(gameId: number) {
+    // Get the userId from the route parameters
+    const userId = this.route.snapshot.params['userId'];
+    
+    if (!userId) {
+      console.error('User ID is not available');
+      return;
+    }
+    
+    this.gameService.delete(userId, gameId).subscribe(res => {
+      // Remove the deleted game from the games array
+      this.games = this.games.filter(game => game.gameId !== gameId);
+      // Update filtered games as well
+      this.filteredGames = this.filteredGames.filter(game => game.gameId !== gameId);
+      
+      // Reset current game if it was the deleted one
+      if (this.currentGame && this.currentGame.gameId === gameId) {
+        this.currentGame = undefined;
+        this.currentIndex = -1;
+      }
+    });
   }
 
   ngOnDestroy() {
