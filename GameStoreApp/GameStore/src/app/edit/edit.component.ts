d1b3e73b23fb55bdb4cdfdfbf227d5458ed135ce
Tevin Gray <tjgray94@yahoo.com>
2025-06-17 13:53:00 -0500

    Updated route for Back to Games button, ngOnInit and submit method for edit page

--- GameStoreApp/GameStore/src/app/edit/edit.component.ts
+++ GameStoreApp/GameStore/src/app/edit/edit.component.ts
@@ -3,6 +3,7 @@ import { GameService } from '../game.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Game } from '../game';
 import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-edit',
@@ -13,14 +14,29 @@ export class EditComponent implements OnInit {
 
   id!: number;
   game!: Game;
+  userId!: number;
   @ViewChild('gamesForm') form!: NgForm;
 
   constructor(private gameService: GameService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['gameId'];
+    
+    // Get userId from route parameters first
+    const routeUserId = this.route.snapshot.params['userId'];
+    if (routeUserId) {
+      this.userId = routeUserId;
+    } else {
+      // Fallback to current user from auth service
+      const currentUser = this.authService.currentUser;
+      if (currentUser && currentUser.userId) {
+        this.userId = currentUser.userId;
+      }
+    }
+    
     this.gameService.getGame(this.id).subscribe(data => {
       this.game = data;
     })
@@ -28,8 +44,8 @@ export class EditComponent implements OnInit {
 
   submit(){
     const gameData = this.game;
-    this.gameService.update(this.id, gameData).subscribe(res => {
-      this.router.navigateByUrl('/api/games')
+    this.gameService.update(this.userId, this.id, gameData).subscribe(res => {
+      this.router.navigate(['/user', this.userId, 'games']);
     })
   }
 }
