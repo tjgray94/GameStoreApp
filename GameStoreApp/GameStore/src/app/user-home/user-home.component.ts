7fc1828b3adbc6c7cc94decd2565e3737b95aee9
Tevin Gray <tjgray94@yahoo.com>
2025-06-17 14:35:58 -0500

    Updated user-home page design

--- GameStoreApp/GameStore/src/app/user-home/user-home.component.ts
+++ GameStoreApp/GameStore/src/app/user-home/user-home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { Game } from '../game';
+import { GameService } from '../game.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-user-home',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-home.component.css']
 })
 export class UserHomeComponent implements OnInit {
+  
+  id!: number;
+  games!: Game[];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              public router: Router, 
+              private gameService: GameService,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['userId'];
+    this.gameService.getGamesByUserId(this.id).subscribe(data => {
+      this.games = data;
+      console.log(data);
+    },
+    error => {
+      console.log(error);
+    })
   }
 
+  viewInventory() {
+    this.router.navigate([`/user/${this.id}/games`]);
+  }
 }
