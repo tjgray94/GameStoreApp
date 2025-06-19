684ada1d4a696c6b777f95d02acfec613bef7a95
Tevin Gray <tjgray94@yahoo.com>
2025-06-19 14:24:07 -0500

    Removed unused routes from app-routing

--- GameStoreApp/GameStore/src/app/app-routing.module.ts
+++ GameStoreApp/GameStore/src/app/app-routing.module.ts
@@ -9,15 +9,10 @@ import { EditComponent } from './edit/edit.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'games', redirectTo: 'api/games', pathMatch: 'full'},
-  { path: 'api/games', component: GameListComponent },
   { path: 'user/:userId/games/add', component: AddgameComponent},
   { path: 'user/:userId/games/:gameId', component: GameDetailComponent },
   { path: 'user/:userId/games', component: GameListComponent },
   { path: 'user/home/:userId' , component: UserHomeComponent },
-  { path: 'api/games/:gameId', component: GameDetailComponent },
-  { path: 'add', component: AddgameComponent},
-  { path: 'api/games/:gameId/edit', component: EditComponent},
   { path: 'user/:userId/games/:gameId/edit', component: EditComponent },
   { path: 'newUser', component: AdduserComponent},
   { path: 'login', component: LoginComponent}
